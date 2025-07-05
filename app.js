ebb2ecfb7b02d9fe1cc477f414e5e38962874123
Federico Giovannitti <fredgiov@udel.edu>
2025-07-05 16:01:47 -0400

    test app routing

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('👋 Hello, Panslonuvo!');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
