c3f9ecbadc9533b34941000c5381fb7acc0226a9
Federico Giovannitti <fredgiov@udel.edu>
2025-07-05 20:57:19 -0400

    --

--- routes/public.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// home page
-router.get('/', (req, res) => {
-  res.render('pages/home', { title: 'Panslonuvo' });
-});
-
-module.exports = router;
